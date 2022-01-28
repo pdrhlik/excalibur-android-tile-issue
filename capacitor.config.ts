cdd92c0fb4545b7ae8a7c90da3f42383b07ae39c
Patrik Drhlík <patrioscraper@gmail.com>
2022-01-28 15:49:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'excalibur-android-tile-issue',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
