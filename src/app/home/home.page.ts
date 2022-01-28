9ea4cce2693cc7ad65fd458e1e0988b81b1e8ea8
Patrik Drhlík <patrioscraper@gmail.com>
2022-01-28 16:07:25 +0100

    Add deps and reproducible issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TiledMapResource } from '@excaliburjs/plugin-tiled';
+import { Color, DisplayMode, Engine, Input, Loader, Vector } from 'excalibur';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    this.initTileGame();
+  }
+
+  initTileGame() {
+    const game = new Engine({
+      backgroundColor: Color.fromHex('#000000'),
+      displayMode: DisplayMode.FillScreen,
+      maxFps: 30,
+      antialiasing: false,
+      suppressConsoleBootMessage: true,
+      pointerScope: Input.PointerScope.Document,
+      canvasElementId: "main-game",
+      snapToPixel: true,
+      suppressHiDPIScaling: true
+    });
+
+    const tiledMap = new TiledMapResource("assets/samplemap/samplemap.tmx");
+    tiledMap.convertPath = function (originPath, relativePath) {
+      return "/assets/samplemap/" + relativePath;
+    }
+
+    let loader = new Loader([tiledMap]);
+
+    game.start(loader).then(function() {
+      tiledMap.addTiledMapToScene(game.currentScene);
+      game.currentScene.camera.move(new Vector(500, 1000), 0);
+    });
+  }
 
 }
